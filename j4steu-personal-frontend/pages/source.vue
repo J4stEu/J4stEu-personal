2d0acc8195f15c509caf90e2200da12c880ce30c
j4steu <NIKKst99@yandex.ru>
2022-01-07 21:17:07 +0300

    - Source positioning fix for mobile

--- j4steu-personal-frontend/pages/source.vue
+++ j4steu-personal-frontend/pages/source.vue
@@ -57,7 +57,7 @@ export default {
       height: auto;
     }
   }
-  section {
+  div {
     @media screen and (min-width: 0px) and (max-width: 768px) {
       justify-content: flex-start !important;
     }
